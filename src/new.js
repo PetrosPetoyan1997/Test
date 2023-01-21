4adb717c00a9524dd75ee73ffdddf0e9efe6a525
Petros1997 <petros.petoyan@caiman.am>
2023-01-21 22:22:51 +0400

    added new consoles, hereee

--- src/new.js
+++ src/new.js
@@ -1,1 +1,19 @@
 console.log('hello world');
+
+
+
+// add new consoles 
+
+
+
+
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');
+console.log('777777777');

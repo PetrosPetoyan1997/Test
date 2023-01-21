83d3e312b42ba42e4eaaedb344666d9405f1a5d7
Petros1997 <petros.petoyan@caiman.am>
2023-01-21 19:42:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { observer } from "mobx-react-lite";
 
+import { makeObservable, observable, computed, autorun , runInAction} from "mobx"
+
+class OrderLine {
+    price = 0
+    amount = 1
+
+    constructor(price) {
+        makeObservable(this, {
+            price: observable,
+            amount: observable,
+            total: computed
+        })
+        this.price = price
+    }
+
+    get total() {
+        console.log("Computing...")
+        return this.price * this.amount
+    }
+}
+
+const order = new OrderLine(0)
+
+const stop = autorun(() => {
+    console.log("Total: " + order.total)
+})
+
+stop()
+
+
+console.log(order.total)
+console.log(order.total)
+console.log(order.total)
+console.log(order.total)
+
+
+// Neither the computation nor autorun will be recomputed.
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
-export default App;
+export default observer(App);

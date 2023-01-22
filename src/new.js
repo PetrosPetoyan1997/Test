d43b97f9852ad5d01f0520a6a3cdc03d2002c24b
Petros1997 <petros.petoyan@caiman.am>
2023-01-22 16:59:24 +0400

    new message 22222

--- src/new.js
+++ src/new.js
@@ -26,3 +26,16 @@ console.log('1111111111');
 console.log('1111111111');
 console.log('1111111111');
 console.log('1111111111');
+
+
+/* ------------------------ */
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
+console.log('1111111111');
